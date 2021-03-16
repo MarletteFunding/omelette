a2be6b47120349373498068b0c68d85c74799f00
Ryan Whitten <ryan.whitten@bestegg.com>
2021-03-16 12:10:13 -0400

    Update sftp to allow auth timeout setting

--- omelette/eggs/sftp.py
+++ omelette/eggs/sftp.py
@@ -14,8 +14,8 @@ class Sftp(pysftp.Connection):
 
     def __init__(self, *, host: str, port: int = 22, username: str, password: Optional[str] = None,
                  private_key: Optional[str] = None, passphrase: Optional[str] = None,
-                 host_fingerprint: Optional[str] = None, **kwargs):
-        cnopts = None
+                 host_fingerprint: Optional[str] = None, auth_timeout: Optional[int] = None):
+        self._cnopts = pysftp.CnOpts()
 
         if private_key:
             private_key = pysftp.RSAKey.from_private_key(StringIO(private_key), passphrase)
@@ -24,21 +24,28 @@ class Sftp(pysftp.Connection):
         if os.getenv("PROJECT_ENV", "local") != "local":
             fingerprint = host_fingerprint.encode()
             host_key = pysftp.RSAKey(data=decodebytes(fingerprint))
-            cnopts = pysftp.CnOpts()
-            cnopts.hostkeys.add(host, 'ssh-rsa', host_key)
+            self._cnopts.hostkeys.add(host, 'ssh-rsa', host_key)
 
-        super(Sftp, self).__init__(host=host,
-                                   username=username,
-                                   password=password,
-                                   private_key=private_key,
-                                   port=int(port),
-                                   cnopts=cnopts)
+        # starting point for transport.connect options
+        self._host = host
+        self._tconnect = {'username': username, 'password': password,
+                          'hostkey': None, 'pkey': None}
+        self._default_path = None
+
+        # check that we have a hostkey to verify
+        if self._cnopts.hostkeys is not None:
+            self._tconnect['hostkey'] = self._cnopts.get_hostkey(host)
+
+        self._sftp_live = False
+        self._sftp = None
+        self._set_username()
+        self._set_logging()
+        # Begin the SSH transport.
+        self._transport = None
+        self._start_transport(host, port)
+        self._transport.use_compression(self._cnopts.compression)
+        self._set_authentication(password, private_key, passphrase)
+        self._transport.auth_timeout = auth_timeout
+        self._transport.connect(**self._tconnect)
 
         logger.info("Connected to sftp.")
-
-    def close(self):
-        # Best effort
-        try:
-            super(Sftp, self).close()
-        except Exception as e:
-            logger.exception(e)
